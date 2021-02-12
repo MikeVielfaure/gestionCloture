0d47a6e434d08d181e03f45c295ea97059ff5d47
Mike Vielfaure <vielfauremike@gmail.com>
2021-02-12 10:24:23 +0100

    tache 2 fin

--- gestionCloture/gestionCloture/GestionDate.cs
+++ gestionCloture/gestionCloture/GestionDate.cs
@@ -8,7 +8,7 @@ namespace gestionCloture
 {
   public abstract class GestionDate
   {
-    // renvoit le numéro du mois précédent à la date d'aujourd'hui sous forme "xx"
+    // renvoit le numéro du mois précédent la date d'aujourd'hui sous forme "xx"
     public static string getMoisPrecedent()
     {
       // récupération de la date d'aujourd'hui
@@ -20,20 +20,62 @@ namespace gestionCloture
     }
 
 
-    // renvoit le numéro du mois précédent à la date en paramètre sous forme "xx"
+    // renvoit le numéro du mois precedent la date en paramètre sous forme "xx"
     public static string getMoisPrecedent(DateTime laDate)
     {
       // récupère le mois
       int leMois = laDate.Month;
-      // récupére et retourne le mois précédent
+      // récupére et retourne le mois precedent
       return moisPrecedent(leMois);
     }
 
 
-    // méthode qui récupère le mois précédent sous forme "xx"
+    // renvoit le numéro du mois suivant la date d'aujourd'hui sous forme "xx"
+    public static string getMoisSuivant()
+    {
+      // récupération de la date d'aujourd'hui
+      DateTime laDate = DateTime.Today;
+      // récupère le mois
+      int leMois = laDate.Month;
+      // récupére et retourne le mois suivant
+      return moisSuivant(leMois);
+    }
+
+
+    // renvoit le numéro du mois suivant la date en paramètre sous forme "xx"
+    public static string getMoisSuivant(DateTime laDate)
+    {
+      // récupère le mois
+      int leMois = laDate.Month;
+      // récupére et retourne le mois suivant
+      return moisSuivant(leMois);
+    }
+
+
+    // vérifie si le jour d'aujourd'hui se situ entre les deux jours mis en paramètre
+    public static bool entre(int jour1, int jour2)
+    {
+      // récupère le jour d'aujourd'hui
+      DateTime laDate = DateTime.Today;
+      int leJour = laDate.Day;
+      // vérifie et retourne si le jour se situ entre les deux jours en paramètre
+      return (leJour >= jour1 && leJour <= jour2) ;
+    }
+
+
+    public static bool entre(int jour1, int jour2, DateTime laDate)
+    {
+      // récupère le jour de la date en paramètre
+      int leJour = laDate.Day;
+      // vérifie et retourne si le jour se situ entre les deux jours en paramètre
+      return (leJour >= jour1 && leJour <= jour2);
+    }
+
+
+    // méthode qui retourne le mois précédent de celui mis en paramètre sous forme "xx"
     private static String moisPrecedent(int leMois)
     {
-      // vérifie si le mois actuel est janvier dans ce cas faudra le mois précédent sera décembre donc 12
+      // vérifie si le mois est janvier dans ce cas faudra le mois précédent sera décembre donc 12
       if (leMois == 1)
       {
         return "12";
@@ -48,6 +90,29 @@ namespace gestionCloture
         else
         {
           return (leMois - 1).ToString();
+        }
+      }
+    }
+
+
+    // méthode qui retourne le mois suivant de celui mis en^paramètre sous forme "xx"
+    private static String moisSuivant(int leMois)
+    {
+      // vérifie si le mois est décembre dans ce cas faudra le mois suivant sera janvier donc 01
+      if (leMois == 1)
+      {
+        return "01";
+      }
+      else
+      {
+        // vérifie si le mois suivant est antérieur à octobre dans ce cas on rajoute un '0' devant
+        if (leMois+1 < 10)
+        {
+          return ("0" + (leMois + 1).ToString());
+        }
+        else
+        {
+          return (leMois + 1).ToString();
         }
       }
     }
--- gestionCloture/gestionClotureTest/UnitTest1.cs
+++ gestionCloture/gestionClotureTest/UnitTest1.cs
@@ -4,20 +4,53 @@ using gestionCloture;
 
 namespace gestionClotureTest
 {
-    [TestClass]
-    public class UnitTest1
+  [TestClass]
+  public class UnitTest1
+  {
+    // test getMoisPrecedent()
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-          Assert.AreEqual(GestionDate.getMoisPrecedent(), "01");
-        }
+      Assert.AreEqual(GestionDate.getMoisPrecedent(), "01");
+    }
 
-         [TestMethod]
-        public void TestMethod2()
-        {
-          DateTime laDate = DateTime.Today;
-          Assert.AreEqual(GestionDate.getMoisPrecedent(laDate), "01");
-        }
+    // test getMoisPrecedent(DateTime la date)
+    [TestMethod]
+    public void TestMethod2()
+    {
+      DateTime laDate = DateTime.Today;
+      Assert.AreEqual(GestionDate.getMoisPrecedent(laDate), "01");
+    }
+
+    // test getMoisSuivant()
+    [TestMethod]
+    public void TestMethod3()
+    {
+      Assert.AreEqual(GestionDate.getMoisSuivant(), "03");
+    }
+
+    // test getMoisSuivant()
+    [TestMethod]
+    public void TestMethod4()
+    {
+      DateTime laDate = DateTime.Today;
+      Assert.AreEqual(GestionDate.getMoisSuivant(laDate), "03");
+    }
+
+    // test entre(int jour1, int jour2)
+    [TestMethod]
+    public void TestMethod5()
+    {
+      Assert.IsTrue(GestionDate.entre(5, 15));
+    }
+
+    // test entre(int jour1, int jour2? DateTime laDate)
+    [TestMethod]
+    public void TestMethod6()
+    {
+      DateTime laDate = DateTime.Today;
+      int leJour = laDate.Day;
+      Assert.IsTrue(GestionDate.entre(5, 15));
+    }
   }
 }
